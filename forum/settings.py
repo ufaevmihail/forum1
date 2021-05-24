ab55a98219200f67c5ce7ae96f379758bbe0a34d
ufaevmihail <michael.ufaev@yandex.ru>
2021-05-24 13:21:43 +0300

    fff

--- forum/settings.py
+++ forum/settings.py
@@ -136,10 +136,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 STATIC_URL = '/static/'
+'''STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]'''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
